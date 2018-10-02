23206b2c9fa54b7f9598c7d8588c73dd245e7265
BT_1N3_25 <vli8558@bths.edu>
2018-10-02 09:52:28 -0400

    Circle + Ellipse Comments

--- src/Ellipse.java
+++ src/Ellipse.java
@@ -3,11 +3,17 @@ public class Ellipse extends Shape {
     private int axisB;
     public Ellipse(int axisA,int axisB)
     {
+        /**
+         * Unlike the Circle, the ellipse requires 2 parameters, 1 for the major and 1 for the minor axis.
+         */
         super("Ellipse");
         this.axisA = axisA;
         this.axisB = axisB;
     }
-
+    /**
+     * The below returns the area and the circumference of the circle.
+     * @return
+     */
     public double getArea()
     {
         return Math.PI *axisB * axisA;
